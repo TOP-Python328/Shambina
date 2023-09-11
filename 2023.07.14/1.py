187406bc3c4dbbc7875fdc061ac43c87191644a1
SvetSham <szxd2009@yandex.ru>
2023-09-11 20:17:02 +0300

    Выполнила домашнее задание к 14.07

---
+++ 2023.07.14/1.py
@@ -0,0 +1,15 @@
+a=float(input())
+b=float(input())
+c=float(input())
+sum=0
+if a>0:
+    sum+=a
+if b>0:
+    sum+=b
+if c>0:
+    sum+=c
+print(sum)
+#5.5
+#-3
+#4
+#9.5
---
+++ 2023.07.14/6.py
@@ -0,0 +1,25 @@
+c1=input()
+c2=input()
+vert_ok=False
+horiz_ok=False
+if abs(int(c1[1])-int(c2[1]))<2:
+    vert_ok=True
+if abs(ord(c1[0])-ord(c2[0]))<2:
+    horiz_ok=True
+if vert_ok and horiz_ok:
+    print("да")
+else:
+    print("нет")
+
+#a1
+#b1
+#да
+
+#a1
+#c1
+#нет
+
+#b2
+#d3
+#нет
+    
